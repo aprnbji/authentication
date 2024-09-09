713126f9315463b82f512985af75cbed9c7919be
aprnbji <aparnap.balaji@gmail.com>
2024-09-09 14:51:39 +0530

    add

--- dependencies.py
+++ dependencies.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyrealsense2 as rs
 import face_recognition
 import dlib
-import mediapipe as mp
 import os
-from scipy.spatial import distance
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
